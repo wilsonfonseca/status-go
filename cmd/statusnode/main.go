1c9d2954faf7dc59be1f4027c7c4e6fad55fecf7
Igor Mandrigin <i@mandrigin.ru>
2019-04-10 16:13:22 +0200

    add templates and QR generation

--- cmd/statusnode/main.go
+++ cmd/statusnode/main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("./cmd/statusnode/assets/*")
+	r.LoadHTMLGlob("./cmd/statusnode/assets/tmpl/*")
+	r.Static("static", "./cmd/statusnode/assets/static")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Enabled": true,
+			"StaticPeers": []string{
+				"one", "two", "tree",
+			},
+			"EnodeID":  "enode://ffffffffffffffff:pAss@127.0.0.1:99999",
+			"MaxPeers": 30,
+		})
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
